fa2297dd5f9b3d33bc7dc2a8d813cc32e40616c0
sm <smcmillan01@gmail.com>
2014-09-09 13:53:14 -0700

    Update to test

--- ex14c.rb
+++ ex14c.rb
@@ -19,3 +19,9 @@ Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
 And you have a #{computer} computer. Nice. 
 """
+
+puts '''
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice. 
+'''
